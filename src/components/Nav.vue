c9c6ceb0fcd50c60e6a2d3ab49f2d9b27a27761e
wuwenbang <wuwenbang@foxmail.com>
2020-07-03 08:58:27 +0800

    完成scss全局变量，全局样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -19,6 +19,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -37,7 +39,7 @@ nav {
     }
   }
   > .item.selected {
-    color: #41b883;
+    color: $color-highlight;
   }
 }
 </style>
