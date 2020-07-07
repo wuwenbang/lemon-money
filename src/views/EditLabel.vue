5fb7903b0e548d89279e8f9b2601ac077c7810aa
wuwenbang <wuwenbang@foxmail.com>
2020-07-07 19:55:30 +0800

    添加EditLabel组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -5,8 +5,21 @@
 <script lang='ts'>
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
+import tagModel from "../models/tagModel";
 @Component
-export default class EditLabel extends Vue {}
+export default class EditLabel extends Vue {
+  created() {
+    const id = this.$route.params.id;
+    tagModel.fetch();
+    const tags = tagModel.data;
+    const tag = tags.filter(t => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
 
 <style lang='scss' scoped >
